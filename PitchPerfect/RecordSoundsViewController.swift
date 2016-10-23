995c4892610d8bc802607f8b0986ad1390121877
Celia Gómez de Villavedón Pedrosa <celiagomezdev@gmail.com>
2016-10-23 13:10:31 +0200

    Delete didReceiveMemoryWarning function

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -28,10 +28,6 @@ class RecordSoundsViewController: UIViewController , AVAudioRecorderDelegate {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     // MARK: Record function
     @IBAction func recordAudio(_ sender: AnyObject) {
